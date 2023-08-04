7ecd351bc97689df500ecec0ee5f0d0b4fdb52f2
hirakero <k16.h.white@gmail.com>
2023-08-05 06:10:16 +0900

    Building the board

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -10,8 +10,35 @@
 
 (def click-count (r/atom 0))
 
+#_(defn game []
+  [:div.game
+   [:dib.game-board
+    [board]
+    [:div.game-info
+     [:div "status"]
+     [:ol "todo"]]]])
+
+#_(defn sqare [& {:keys [value on-click]}]
+  [:button.square {:on-click on-click} value])
+(defn board []
+ [:div
+  [:div.board-row
+   [:button.square "1"]
+   [:button.square "2"]
+   [:button.square "3"]]
+  [:div.board-row
+   [:button.square "4"]
+   [:button.square "5"]
+   [:button.square "6"]]
+  [:div.board-row
+   [:button.square "7"]
+   [:button.square "8"]
+   [:button.square "9"]]]
+  )
+
 (defn hello []
   [:<>
    [:p "Hello, react-tutorial is running!"]
    [:p "Here's an example of using a component with state:"]
-   [click-counter click-count]])
+   [click-counter click-count]
+   [board]])
