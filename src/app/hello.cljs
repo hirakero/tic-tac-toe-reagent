005ebc52dd9c1b9b02cef179ab66f52572b41846
hirakero <k16.h.white@gmail.com>
2023-08-06 13:05:14 +0900

    Lifting state up, again. 途中

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -1,14 +1,6 @@
 (ns app.hello
   (:require [reagent.core :as r]))
 
-#_(defn game []
-  [:div.game
-   [:dib.game-board
-    [board]
-    [:div.game-info
-     [:div "status"]
-     [:ol "todo"]]]])
-
 (defn square [& {:keys [value on-click]}] 
   [:button.square
    {:on-click on-click}
@@ -35,34 +27,43 @@
   (calcurate-winner [:o nil nil nil :o nil nil nil :o]) 
   (calcurate-winner [nil nil nil nil :o nil nil nil :o]))
 
-(defn board []
+(defn board [& {:keys [state]}]
+  (fn []
+    (let [{:keys [squares x-is-next?]} @state
+          handle-click (fn [i]
+                         (when-not (or (squares i)
+                                       (calcurate-winner squares))
+                           (swap! state assoc-in [:squares i] (if x-is-next? "x" "o"))
+                           (swap! state update :x-is-next? not)))
+          winner (calcurate-winner squares)
+          status (if winner
+                   (str "Winner:" winner)
+                   (str "Next player:" (if x-is-next? "x" "o")))]
+      [:<>
+       [:div.status status]
+       [:div.board-row
+        [square :value (squares 0) :on-click #(handle-click 0)]
+        [square :value (squares 1) :on-click #(handle-click 1)]
+        [square :value (squares 2) :on-click #(handle-click 2)]]
+       [:div.board-row
+        [square :value (squares 3) :on-click #(handle-click 3)]
+        [square :value (squares 4) :on-click #(handle-click 4)]
+        [square :value (squares 5) :on-click #(handle-click 5)]]
+       [:div.board-row
+        [square :value (squares 6) :on-click #(handle-click 6)]
+        [square :value (squares 7) :on-click #(handle-click 7)]
+        [square :value (squares 8) :on-click #(handle-click 8)]]])))
+
+(defn game []
   (let [state (r/atom {:squares (vec (repeat 9 nil))
                        :x-is-next? true})]
-    (fn []
-      (let [handle-click (fn [i]
-                           (when-not (or (get-in @state [:squares i])
-                                         (calcurate-winner (:squares @state)))
-                             (swap! state assoc-in [:squares i] (if (get @state :x-is-next?) "x" "o"))
-                             (swap! state update :x-is-next? not)))
-            winner (calcurate-winner (:squares @state))
-            status (if winner
-                     (str "Winner:" winner)
-                     (str "Next player:" (if (:x-is-next? @state) "x" "o")))]
-        [:<>
-         [:div.status status]
-         [:div.board-row
-          [square :value (get-in @state [:squares 0]) :on-click #(handle-click 0)]
-          [square :value (get-in @state [:squares 1]) :on-click #(handle-click 1)]
-          [square :value (get-in @state [:squares 2]) :on-click #(handle-click 2)]]
-         [:div.board-row
-          [square :value (get-in @state [:squares 3]) :on-click #(handle-click 3)]
-          [square :value (get-in @state [:squares 4]) :on-click #(handle-click 4)]
-          [square :value (get-in @state [:squares 5]) :on-click #(handle-click 5)]]
-         [:div.board-row
-          [square :value (get-in @state [:squares 6]) :on-click #(handle-click 6)]
-          [square :value (get-in @state [:squares 7]) :on-click #(handle-click 7)]
-          [square :value (get-in @state [:squares 8]) :on-click #(handle-click 8)]]]))))
+    [:div.game 
+     [:div.game-board
+      [board :state state]
+      [:div.game-info
+       [:div "status"]
+       [:ol "todo"]]]]))
 
 (defn hello []
   [:<>
-   [board]])
+   [game]])
